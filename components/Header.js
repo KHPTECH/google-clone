39e6def1c1098b16ccea0c6d9b89f3398a38c445
KHPTECH <khptechnics56@gmail.com>
2021-08-12 13:29:59 +0530

    app

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Image from "next/Image";
+import Image from "next/image";
 import { useRouter } from "next/dist/client/router";
 import { useRef } from "react";
 import { XIcon , MicrophoneIcon, SearchIcon} from "@heroicons/react/solid";
